a43d438bca93843dc3f2dd383652322d47070dc7
mayar younesch <128980800+MayarYCh@users.noreply.github.com>
2023-05-12 20:49:08 +0100

    Delete response.blade.php

--- resources/views/response.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="{{ route('microtasks.storeResponse') }}" method="POST">
-    @csrf
-    <input type="hidden" name="microtask_id" value="{{ $microtask->id }}">
-    <textarea name="response" rows="5">{{ $microtask->response }}</textarea>
-    <button type="submit">Submit</button>
-</form>
